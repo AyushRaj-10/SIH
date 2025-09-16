b3a867b3419f038f074bb6aab13e7f8b6615f227
AyushRaj-10 <ayushrajranjan10@gmail.com>
2025-09-16 22:42:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,24 +10,17 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://sih-sigma-dun.vercel.app" 
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "https://sih-sigma-dun.vercel.app" 
+// ];
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log(`🚫 CORS blocked request from: ${origin}`); // helpful for debugging
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+app.options('*', cors({
+  origin: allowedOrigins,
   credentials: true,
 }));
 app.use(cookieParser());
